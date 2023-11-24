28d83525194e634b101d690e7314d71be09c551d
HENG SOPHANHA <141711644+HENGSOPHANHA@users.noreply.github.com>
2023-11-24 21:56:18 +0700

    Update app.py

--- app.py
+++ app.py
@@ -11,12 +11,12 @@ data = pd.read_csv("traineddata.csv")
 
 st.title("Laptop Price Predictor")
 
-company_mapping = {'Apple': 0, 'Dell': 1, 'HP': 2, 'Acer': 3, 'Lenovo': 4, 'Asus': 5, 'MSI': 6, 'Toshiba': 7, 'Razer': 8, 'Mediacom': 9}
+company_mapping = {'Apple': 0, 'Dell': 1, 'HP': 2, 'Acer': 3, 'Lenovo': 4, 'Asus': 5, 'MSI': 6, 'Toshiba': 7, 'Razer': 8, 'Mediacom': 9, 'Chuwi': 10}
 type_mapping = {'Ultrabook': 0, 'Notebook': 1, 'Netbook': 2, 'Gaming': 3, '2 in 1 Convertible': 4, 'Workstation': 5}
 os_mapping = {'Mac': 0, 'Windows': 1, 'Linux': 2, 'Chrome OS': 3, 'No OS': 4}
 cpu_mapping = {'Intel Core i5': 0, 'Intel Core i7': 1, 'Intel Core i3': 2, 'Intel Celeron Dual Core': 3, 'Intel Core i6': 4}
 
-company = st.selectbox('Brand', data['Company'].unique(), format_func=lambda x: company_mapping[x])
+company = st.selectbox('Brand', data['Company'].unique(), format_func=lambda x: company_mapping.get(x, x))
 type = st.selectbox('Type', data['TypeName'].unique(), format_func=lambda x: type_mapping[x])
 ram = st.selectbox('Ram(in GB)', [2, 4, 6, 8, 12, 16, 24, 32, 64])
 os = st.selectbox('OS', data['OpSys'].unique(), format_func=lambda x: os_mapping[x])
@@ -47,7 +47,7 @@ if st.button('Predict Price'):
 
     ppi = ((X_resolution**2) + (Y_resolution**2))**0.5 / (screen_size)
 
-    query = np.array([company_mapping[company], type_mapping[type], ram, weight,
+    query = np.array([company_mapping.get(company, company), type_mapping[type], ram, weight,
                       touchscreen, ips, ppi, cpu_mapping[cpu], hdd, ssd, gpu, os_mapping[os]])
 
     query = query.reshape(1, 12)
