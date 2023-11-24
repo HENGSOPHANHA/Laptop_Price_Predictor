10d4a11f27cbe7ae0c6c36895956ef02e83d6f78
HENG SOPHANHA <141711644+HENGSOPHANHA@users.noreply.github.com>
2023-11-24 21:36:19 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,14 +7,6 @@ file1 = open('pipe.pkl', 'rb')
 rf = pickle.load(file1)
 file1.close()
 
-file2 = open('df.pkl', 'rb')
-rf = pickle.load(file2)
-file2.close()
-
-file3 = open('laptoppricepredictor.pkl', 'rb')
-rf = pickle.load(file3)
-file3.close()
-
 # Apple,Ultrabook,8,Mac,1.37,0,1,226.98300468106115,Intel Core i5,0,128,Intel
 
 data = pd.read_csv("traineddata.csv")
