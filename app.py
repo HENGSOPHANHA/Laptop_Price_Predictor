2d5cba268525bb94b5c3504e28d97fae967c0251
HENG SOPHANHA <141711644+HENGSOPHANHA@users.noreply.github.com>
2023-11-25 18:04:36 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,13 @@
 import streamlit as st
-import pickle
+import pandas as pd
 import numpy as np
-import joblib
-import pandas as pd  # Add this import for pd
+import pickle
 
-# Load the model
-pipe = pickle.load(open('pipe.pkl', 'rb'))
+file1 = open('pipe.pkl', 'rb')
+rf = pickle.load(file1)
+file1.close()
 
-# Load or create the dataframe
-df_path = 'df.pkl'
-
-try:
-    df = joblib.load(df_path)
-except FileNotFoundError:
-    # If the file is not found, create an empty dataframe
-    df = pd.DataFrame()
-
-st.title("Laptop Predictor")
-
-#Apple,Ultrabook,8,Mac,1.37,0,1,226.98300468106115,Intel Core i5,0,128,Intel
+# Apple,Ultrabook,8,Mac,1.37,0,1,226.98300468106115,Intel Core i5,0,128,Intel
 
 data = pd.read_csv("traineddata.csv")
 
@@ -26,44 +15,59 @@ data['IPS'].unique()
 
 st.title("Laptop Price Predictor")
 
-# brand
-company = st.selectbox('Brand',df['Company'].unique())
+company = st.selectbox('Brand', data['Company'].unique())
+
+
 
 # type of laptop
-type = st.selectbox('Type',df['TypeName'].unique())
 
-# Ram
-ram = st.selectbox('RAM(in GB)',[2,4,6,8,12,16,24,32,64])
+type = st.selectbox('Type', data['TypeName'].unique())
 
-# weight
-weight = st.number_input('Weight of the Laptop')
+# Ram present in laptop
 
-# Touchscreen
-touchscreen = st.selectbox('Touchscreen',['No','Yes'])
+ram = st.selectbox('Ram(in GB)', [2, 4, 6, 8, 12, 16, 24, 32, 64])
+
+# os of laptop
+
+os = st.selectbox('OS', data['OpSys'].unique())
+
+# weight of laptop
+
+weight = st.number_input('Weight of the laptop')
+
+# touchscreen available in laptop or not
+
+touchscreen = st.selectbox('Touchscreen', ['No', 'Yes'])
 
 # IPS
-ips = st.selectbox('IPS',['No','Yes'])
+
+ips = st.selectbox('IPS', ['No', 'Yes'])
 
 # screen size
+
 screen_size = st.number_input('Screen Size')
 
-# resolution
-resolution = st.selectbox('Screen Resolution',['1920x1080','1366x768','1600x900','3840x2160','3200x1800','2880x1800','2560x1600','2560x1440','2304x1440'])
+# resolution of laptop
 
-#cpu
-cpu = st.selectbox('CPU',df['Cpu brand'].unique())
+resolution = st.selectbox('Screen Resolution', [
+                          '1920x1080', '1366x768', '1600x900', '3840x2160', '3200x1800', '2880x1800', '2560x1600', '2560x1440', '2304x1440'])
 
-hdd = st.selectbox('HDD(in GB)',[0,128,256,512,1024,2048])
+# cpu
 
-ssd = st.selectbox('SSD(in GB)',[0,8,128,256,512,1024])
+cpu = st.selectbox('CPU', data['CPU_name'].unique())
 
-gpu = st.selectbox('GPU',df['Gpu brand'].unique())
+# hdd
 
-os = st.selectbox('OS',df['os'].unique())
+hdd = st.selectbox('HDD(in GB)', [0, 128, 256, 512, 1024, 2048])
 
+# ssd
+
+ssd = st.selectbox('SSD(in GB)', [0, 8, 128, 256, 512, 1024])
+
+gpu = st.selectbox('GPU(in GB)', data['Gpu brand'].unique())
 
 if st.button('Predict Price'):
-    # query
+
     ppi = None
     if touchscreen == 'Yes':
         touchscreen = 1
@@ -75,31 +79,17 @@ if st.button('Predict Price'):
     else:
         ips = 0
 
-    X_res = int(resolution.split('x')[0])
-    Y_res = int(resolution.split('x')[1])
-    ppi = ((X_res**2) + (Y_res**2))**0.5 / screen_size
+    X_resolution = int(resolution.split('x')[0])
+    Y_resolution = int(resolution.split('x')[1])
 
-    # Creating a DataFrame for the query
-    query_df = pd.DataFrame([{
-        'Company': company,
-        'TypeName': type,
-        'Ram': ram,
-        'Weight': weight,
-        'Touchscreen': touchscreen,
-        'IPS': ips,
-        'PPI': ppi,
-        'Cpu brand': cpu,
-        'HDD': hdd,
-        'SSD': ssd,
-        'Gpu brand': gpu,
-        'os': os
-    }])
+    ppi = ((X_resolution**2)+(Y_resolution**2))**0.5/(screen_size)
 
-    # Concatenate the query DataFrame with the main DataFrame
-    df = pd.concat([df, query_df], ignore_index=True)
+    query = np.array([company, type, ram, weight,
+                      touchscreen, ips, ppi, cpu, hdd, ssd, gpu, os])
 
-    # Assuming 'pipe' is a trained model pipeline
-    prediction = np.exp(pipe.predict(query_df))[0]
+    query = query.reshape(1, 12)
 
-    st.title("The predicted price of this configuration is $" + str(int(prediction)))
+    prediction = int(np.exp(rf.predict(query)[0]))
 
+    st.title("Predicted price for this laptop could be between " +
+             str(prediction-100)+"$" + " to " + str(prediction+100)+"$")
