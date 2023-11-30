81ec8f1c9f34fd7c188f7ef683753c0e22c7a404
HENG SOPHANHA <141711644+HENGSOPHANHA@users.noreply.github.com>
2023-11-30 13:26:32 +0700

    Update app.py

--- app.py
+++ app.py
@@ -21,6 +21,8 @@ file1 = open('pipe.pkl', 'rb')
 rf = pickle.load(file1)
 file1.close()
 
+# Apple,Ultrabook,8,Mac,1.37,0,1,226.98300468106115,Intel Core i5,0,128,Intel
+
 data = pd.read_csv("traineddata.csv")
 
 data['IPS'].unique()
@@ -30,37 +32,48 @@ st.title("Laptop Price Predictor")
 company = st.selectbox('Brand', data['Company'].unique())
 
 # type of laptop
+
 type = st.selectbox('Type', data['TypeName'].unique())
 
 # Ram present in laptop
+
 ram = st.selectbox('Ram(in GB)', [2, 4, 6, 8, 12, 16, 24, 32, 64])
 
 # os of laptop
+
 os = st.selectbox('OS', data['OpSys'].unique())
 
 # weight of laptop
+
 weight = st.number_input('Weight of the laptop')
 
 # touchscreen available in laptop or not
+
 touchscreen = st.selectbox('Touchscreen', ['No', 'Yes'])
 
 # IPS
+
 ips = st.selectbox('IPS', ['No', 'Yes'])
 
 # screen size
+
 screen_size = st.number_input('Screen Size')
 
 # resolution of laptop
+
 resolution = st.selectbox('Screen Resolution', [
                           '1920x1080', '1366x768', '1600x900', '3840x2160', '3200x1800', '2880x1800', '2560x1600', '2560x1440', '2304x1440'])
 
 # cpu
+
 cpu = st.selectbox('CPU', data['CPU_name'].unique())
 
 # hdd
+
 hdd = st.selectbox('HDD(in GB)', [0, 128, 256, 512, 1024, 2048])
 
 # ssd
+
 ssd = st.selectbox('SSD(in GB)', [0, 8, 128, 256, 512, 1024])
 
 gpu = st.selectbox('GPU(in GB)', data['Gpu brand'].unique())
@@ -77,21 +90,17 @@ if st.button('Predict Price'):
     else:
         ips = 0
 
-    X_resolution = int(resolution.split('x')[0])
-    Y_resolution = int(resolution.split('x')[1])
+    X_resolution = float(resolution.split('x')[0])
+    Y_resolution = float(resolution.split('x')[1])
 
-    ppi = ((X_resolution**2)+(Y_resolution**2))**0.5/(screen_size)
+    ppi = ((X_resolution**2) + (Y_resolution**2))**0.5 / (screen_size)
 
-    query = pd.DataFrame(data=[[company, type, ram, weight,
-                      touchscreen, ips, ppi, cpu, hdd, ssd, gpu, os]], columns=data.columns[:-1])
+    query = np.array([company, type, ram, weight,
+                      touchscreen, ips, ppi, cpu, hdd, ssd, gpu, os])
 
-    # Assuming 'preprocessor' is the name of the ColumnTransformer in your pipeline
-    column_transformer = rf.named_steps['Gpu brand']
-
-    # Use the transform method directly on the column transformer
-    query_transformed = column_transformer.transform(query)
-
-    prediction = int(np.exp(rf.named_steps['model'].predict(query_transformed)[0]))
+    query = query.reshape(1, 12)
+    prediction_value = np.array(rf.predict(query))[0]  # Convert to NumPy array
+    prediction = float(np.exp(prediction_value))
 
     st.title("Predicted price for this laptop could be between " +
-             str(prediction - 100) + "$" + " to " + str(prediction + 100) + "$")
+             str(prediction - 10) + "$" + " to " + str(prediction + 10) + "$")
