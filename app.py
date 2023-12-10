76cf0e8baabf8cdc77a9253e185c226cab65f3b9
HENG SOPHANHA <141711644+HENGSOPHANHA@users.noreply.github.com>
2023-12-10 17:22:57 +0700

    Update app.py

--- app.py
+++ app.py
@@ -16,27 +16,24 @@ model = load_model()
 def main():
     st.title('Laptop Price Predictor')
 
-    # Input fields based on the features used in the model
-    # Note: These should be adjusted to match the exact features of your model
-    company = st.selectbox('Company', ['HP', 'Dell', 'Lenovo', 'Asus', 'Acer', 'MSI', 'Toshiba', 'Apple', 'Samsung', 'Razer', 'Microsoft', 'Xiaomi'])
-    type_name = st.selectbox('Type', ['Ultrabook', 'Notebook', 'Gaming', '2 in 1 Convertible', 'Workstation', 'Netbook'])
-    inches = st.number_input('Screen Size (in Inches)', min_value=10.0, max_value=20.0, step=0.1, format="%.1f")
-    screen_resolution = st.selectbox('Screen Resolution', ['1920x1080', '1366x768', '1600x900', '3840x2160', '3200x1800', '2560x1440', '1440x900', '2560x1600', '2304x1440'])
-    cpu = st.text_input('CPU Model (e.g., Intel Core i7)')
-    ram = st.selectbox('RAM (in GB)', [2, 4, 6, 8, 12, 16, 24, 32, 64])
-    memory = st.text_input('Memory (e.g., 512GB SSD)')
-    gpu = st.text_input('GPU Model (e.g., NVIDIA GeForce GTX 1050)')
-    op_sys = st.selectbox('Operating System', ['Windows 10', 'Windows 7', 'Linux', 'Mac OS', 'Chrome OS', 'Windows 10 S', 'No OS', 'Windows 8'])
-    weight = st.number_input('Weight (in kg)', min_value=0.5, max_value=5.0, step=0.1, format="%.1f")
+    # Define the input fields for the features used in the model as selectboxes
+    # Note: The options provided here are placeholders. Replace them with actual options from your dataset.
+    company = st.selectbox('Select Company', ['HP', 'Dell', 'Lenovo', 'Asus', 'Apple', 'Acer', 'Microsoft', 'Toshiba', 'Other'])
+    type_name = st.selectbox('Select Type', ['Ultrabook', 'Notebook', 'Gaming', '2-in-1 Convertible', 'Workstation', 'Netbook', 'Other'])
+    inches = st.selectbox('Select Screen Size (in Inches)', ['10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20'])
+    screen_resolution = st.selectbox('Select Screen Resolution', ['1920x1080', '1366x768', '1600x900', '3840x2160', 'Other'])
+    cpu = st.selectbox('Select CPU Model', ['Intel Core i3', 'Intel Core i5', 'Intel Core i7', 'Intel Core i9', 'AMD Ryzen', 'Other'])
+    ram = st.selectbox('Select RAM (GB)', ['4', '8', '16', '32', '64', 'Other'])
+    memory = st.selectbox('Select Memory', ['128GB SSD', '256GB SSD', '512GB SSD', '1TB SSD', '2TB SSD', 'Other'])
+    gpu = st.selectbox('Select GPU Model', ['NVIDIA', 'AMD Radeon', 'Intel', 'Other'])
+    op_sys = st.selectbox('Select Operating System', ['Windows 10', 'Windows 7', 'Linux', 'MacOS', 'Chrome OS', 'Other'])
+    weight = st.selectbox('Select Weight (kg)', ['1', '1.5', '2', '2.5', '3', '3.5', '4', 'Other'])
 
     # Predict button
     if st.button('Predict Price'):
         # Prepare the feature array for prediction
-        # Note: This part needs to be customized based on how the model expects the input features
+        # Here, you might need to process these features (e.g., encoding) before prediction
         features = np.array([[company, type_name, inches, screen_resolution, cpu, ram, memory, gpu, op_sys, weight]])
-        
-        # Feature preprocessing (if required by the model)
-        # Example: Encoding categorical variables, feature scaling, etc.
 
         # Predicting the price
         predicted_price = model.predict(features)
